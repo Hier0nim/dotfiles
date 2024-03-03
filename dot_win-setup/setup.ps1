5b554ff1b8735cdd045ab210400d0aaebdb30484
Hier0nim <hieronimdaniel@gmail.com>
2024-03-03 15:12:03 +0100

    'win-setup'

---
+++ dot_win-setup/setup.ps1
@@ -0,0 +1,54 @@
+# install latest version of winget
+Add-AppxPackage https://github.com/microsoft/winget-cli/releases/latest/download/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+# Winget installations
+winget install --id=Git.Git --accept-package-agreements --accept-source-agreements
+winget install --id=GitHub.GitHubDesktop --accept-package-agreements --accept-source-agreements
+winget install --id=JetBrains.Rider --accept-package-agreements --accept-source-agreements
+winget install --id=Vifm.Vifm --accept-package-agreements --accept-source-agreements
+winget install --id=Neovim.Neovim --accept-package-agreements --accept-source-agreements
+winget install --id=OpenJS.NodeJS --accept-package-agreements --accept-source-agreements
+winget install --id=twpayne.chezmoi --accept-package-agreements --accept-source-agreements
+winget install --id=Chocolatey.Chocolatey --accept-package-agreements --accept-source-agreements
+winget install --id=glzr-io.glazewm --accept-package-agreements --accept-source-agreements
+
+# Chocolatey installations
+choco install mingw -y
+choco install make -y
+choco install unzip -y
+
+# Initialize dotfiles manager
+chezmoi init --apply Hier0nim
+
+# ---------------------------------------------------------------------------------------------
+# Register Task Scheduler tasks for specified programs to run at user login
+
+# Buttery TaskBar (Update the path as necessary)
+$ButteryTaskBarPath = "$env:USERPROFILE\.win-setup\ButteryTaskBar\buttery-taskbar.exe"
+$ActionButteryTaskBar = New-ScheduledTaskAction -Execute $ButteryTaskBarPath
+$TriggerLogon = New-ScheduledTaskTrigger -AtLogon
+$PrincipalHighest = New-ScheduledTaskPrincipal -UserId "$env:USERDOMAIN\$env:USERNAME" -LogonType Interactive -RunLevel Highest
+
+Register-ScheduledTask -Action $ActionButteryTaskBar -Trigger $TriggerLogon -TaskName "Run Buttery TaskBar at login" -Description "Runs Buttery TaskBar at user login" -Principal $PrincipalHighest
+
+# GlazeWM
+$GlazeWMPath = "$env:USERPROFILE\AppData\Local\Microsoft\WinGet\Packages\glzr-io.glazewm_Microsoft.Winget.Source_8wekyb3d8bbwe\glazewm.exe"
+$ActionGlazeWM = New-ScheduledTaskAction -Execute $GlazeWMPath
+
+Register-ScheduledTask -Action $ActionGlazeWM -Trigger $TriggerLogon -TaskName "Run GlazeWM at login" -Description "Runs GlazeWM at user login" -Principal $PrincipalHighest
+
+# ThrottleStop
+$ThrottleStopPath = "C:\ProgramData\chocolatey\lib\throttlestop\tools\throttlestop\ThrottleStop.exe"
+$ActionThrottleStop = New-ScheduledTaskAction -Execute $ThrottleStopPath
+
+Register-ScheduledTask -Action $ActionThrottleStop -Trigger $TriggerLogon -TaskName "Run ThrottleStop at login" -Description "Runs ThrottleStop at user login" -Principal $PrincipalHighest
+
+# Modify each task to run on battery power
+$taskNames = @("Run Buttery TaskBar at login", "Run GlazeWM at login", "Run ThrottleStop at login")
+
+foreach ($taskName in $taskNames) {
+    $task = Get-ScheduledTask -TaskName $taskName
+    $task.Settings.DisallowStartIfOnBatteries = $false
+    $task.Settings.StopIfGoingOnBatteries = $false
+    Set-ScheduledTask -InputObject $task
+}
