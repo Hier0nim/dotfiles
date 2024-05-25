ee71f29783ee5ed1c7b76576e8f1a4a6b69c938d
Hier0nim <hieronimdaniel@gmail.com>
2024-05-25 02:04:03 +0200

    Update AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1 Update .win-setup/scoop.config

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,35 @@
+# ---------------------------------------------------------------------------------------------
+# Functions 
+# ---------------------------------------------------------------------------------------------
+
+function yy
+{
+  $tmp = [System.IO.Path]::GetTempFileName()
+  yazi $args --cwd-file="$tmp"
+  $cwd = Get-Content -Path $tmp
+  if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path)
+  {
+    Set-Location -LiteralPath $cwd
+  }
+  Remove-Item -Path $tmp
+}
+
+# ---------------------------------------------------------------------------------------------
+# oh-my-posh setup 
+# ---------------------------------------------------------------------------------------------
+
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\hotstick.minimal.omp.json" | Invoke-Expression
+
+# ---------------------------------------------------------------------------------------------
+# Setup 
+# ---------------------------------------------------------------------------------------------
+#
 $PSStyle.FileInfo.Directory = ""
 $env:EDITOR='nvim'
+
+# ---------------------------------------------------------------------------------------------
+# Modules 
+# ---------------------------------------------------------------------------------------------
 
 Import-Module -Name Terminal-Icons
 Import-Module ZLocation
@@ -19,5 +48,10 @@ if ($host.Name -eq 'ConsoleHost')
 }
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+
+# ---------------------------------------------------------------------------------------------
+# Aliases 
+# ---------------------------------------------------------------------------------------------
 
 Set-Alias -Name which -Value Get-Command
