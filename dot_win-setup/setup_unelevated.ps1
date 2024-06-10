e304a39a1446c88204b55a0a0d0329dd2a82dfcc
Hier0nim <hieronimdaniel@gmail.com>
2024-06-10 08:27:05 +0200

    Update .win-setup/setup.ps1 Update .win-setup/setup_unelevated.ps1

--- dot_win-setup/setup_unelevated.ps1
+++ dot_win-setup/setup_unelevated.ps1
@@ -15,6 +15,3 @@ if (Test-Path $scoopPackagesFile)
 {
     Write-Host "Scoop configuration file not found: $scoopPackagesFile" -ForegroundColor Yellow
 }
-
-scoop bucket add CrypticButter https://github.com/CrypticButter/ScoopBucket
-scoop install CrypticButter/buttery-taskbar
